2d3c2379b6ce538c353bedfa3ee9002ed1ac1ee9
Anton Bogolepov <antonsergeevich25@gmail.com>
2018-03-21 21:31:02 +0300

    Добавление Square.cs

---
+++ SquareProj/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Square")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Square")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("401fca0f-fe40-4b70-a2f6-5f2792a29e01")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SquareProj/Square.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square
+{
+    public class Triangle
+    {
+        public static double PerimHalf(double a, double b, double c)
+        {
+            double result = 0.5 * (a + b + c);
+            return result;
+        }
+
+        public static double SquareTriangle(double a, double b, double c)
+        {
+            double p = PerimHalf(a, b, c);
+            return Math.Round(Math.Sqrt(p*(p-a)*(p-b)*(p-c)),3);
+        }
+
+        public static bool IsRightTriangle(double a, double b, double c)
+        {
+            bool isRight;
+            if (a*a == b * b + c * c)
+            {
+                isRight = true;
+            }
+            else if (b*b == a * a + c * c)
+            {
+                isRight = true;
+            }
+            else if (c*c == a*a + b * b)
+            {
+                isRight = true;           
+            }
+            else
+            {
+                isRight = false;
+            }
+
+            return isRight;
+        }
+    }
+
+    public class Circle
+    {
+        public static double SquareCircle(double r)
+        {
+            double pi = Math.PI;
+            double s = Math.Round(2 * pi * r,3);
+            return s;
+        }
+    }
+
+    
+}
