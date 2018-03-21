fbfc8d639371f7504f73967d7e095b05dc02cb98
Anton Bogolepov <antonsergeevich25@gmail.com>
2018-03-21 21:26:17 +0300

    Создание

---
+++ FormForSquare/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormForSquare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = Convert.ToDouble(textBox1.Text);
+                double b = Convert.ToDouble(textBox2.Text);
+                double c = Convert.ToDouble(textBox3.Text);
+
+                textBox4.Text = Square.Triangle.SquareTriangle(a, b, c).ToString("#.##");
+                textBox5.Text = Square.Triangle.PerimHalf(a, b, c).ToString("#.##");
+
+                label2.Text = Square.Triangle.IsRightTriangle(a, b, c).ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Проверьте введенные данные" );
+            }
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double r = System.Double.Parse(textBox6.Text);
+                label1.Text = Square.Circle.SquareCircle(r).ToString("#.##");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Проверьте введенные данные");
+            }
+        }
+    }
+}
---
+++ Square.Tests/SquareTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Square.Tests
+{
+    [TestClass]
+    public class SquareTests
+    {
+        [TestMethod]
+        public void PerimHalf_35and45and55_675returned()
+        {
+            //arrange
+            double x = 3.5;
+            double y = 4.5;
+            double z = 5.5;
+            double expected = 6.75;
+
+
+            //act
+            double actual = Square.Triangle.PerimHalf(x,y,z);
+            //assert
+            Assert.AreEqual(expected, actual); 
+        }
+
+        [TestMethod]
+        public void SquareTriangle_35and45and55_785returned()
+        {
+            //arrange
+            double x = 3.5;
+            double y = 4.5;
+            double z = 5.5;
+            double expected = 7.855;
+
+
+            //act
+            double actual = Square.Triangle.SquareTriangle(x, y, z);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsRightTriangle_3and4and5_TrueReturned()
+        {
+            //arrange
+            double x = 3;
+            double y = 4;
+            double z = 5;
+            bool expected = true;
+
+
+            //act
+            bool actual = Square.Triangle.IsRightTriangle(x, y, z);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SquareCircle_2_1256returned()
+        {
+            //arrange
+            double x = 2;
+
+            double expected = 12.566;
+
+
+            //act
+            double actual = Square.Circle.SquareCircle(x);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
